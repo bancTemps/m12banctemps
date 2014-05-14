3325bfd394e4c59f791fb2befbe888373469c5bf
natupatu <natupatupato@gmail.com>
2014-05-14 18:47:01 +0200

    Navbars igualados
    Los archivos views>admin>dashboard y views>site>layouts>default

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -64,20 +64,21 @@
 	<div class="navbar navbar-default navbar-inverse col-xs-12 col-sm-12 col-md-12">
 		<div class="container">
 	        <div class="navbar-header">
-					<a href="{{{ URL::to('') }}}">
-					<img src="{{asset('img/logo-banc.png')}}" title="Logo">
-					</a>
-	            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-	                <span class="sr-only">Toggle navigation</span>
-	                <span class="icon-bar"></span>
-	                <span class="icon-bar"></span>
-	                <span class="icon-bar"></span>
-	            </button>
+				<a href="{{{ URL::to('') }}}">
+				<!--<img src="{{asset('img/logo-banc.png')}}" title="Logo">-->
+                                    Banc del Temps
+				</a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-	        </div>
+        </div>
 			<div class="collapse navbar-collapse navbar-ex1-collapse">
 
-				<ul class="nav navbar-nav" style="margin-left: 20px;">
+				<!--<ul class="nav navbar-nav" style="margin-left: 20px;">
 					<li{{ (Request::is('admin') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin') }}}"><span class="glyphicon glyphicon-home"></span> Home</a></li>
 					<li{{ (Request::is('admin/blogs*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/blogs') }}}"><span class="glyphicon glyphicon-list-alt"></span> Blog</a></li>
 					<li{{ (Request::is('admin/comments*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/comments') }}}"><span class="glyphicon glyphicon-bullhorn"></span> Comments</a></li>
@@ -104,7 +105,20 @@
 							<li><a href="{{{ URL::to('user/logout') }}}"><span class="glyphicon glyphicon-share"></span> Logout</a></li>
 						</ul>
 					</li>
-				</ul>
+				</ul>-->
+				<ul class="nav navbar-nav pull-right">
+	                <li><a href="{{{ URL::to('blog') }}}">Blog</a></li>
+	                @if (Auth::check())
+	                @if (Auth::user()->hasRole('admin'))
+	                <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
+	                @endif
+	                <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
+	                <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
+	                @else
+	                <li {{(Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
+	                <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('Sign Up') }}}</a></li>
+	                @endif
+	            </ul>
 			</div><!-- ./ nav-collapse -->
         </div><!-- ./ container -->
 	</div><!-- ./ navbar -->
