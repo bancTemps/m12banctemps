a0a075b73847d8563526c3f6ce1d3c198d62a7e0
natupatu <natupatupato@gmail.com>
2014-05-21 17:30:15 +0200

    Cambios en el signup

--- app/controllers/user/UserController.php
+++ app/controllers/user/UserController.php
@@ -38,6 +38,11 @@ class UserController extends BaseController {
      */
     public function postIndex()
     {
+        // Natu
+        $this->user->name = Input::get( 'name' );
+        $this->user->surname = Input::get( 'surname' );
+        // !Natu
+
         $this->user->username = Input::get( 'username' );
         $this->user->email = Input::get( 'email' );
 
@@ -65,14 +70,11 @@ class UserController extends BaseController {
         // Save if valid. Password field will be hashed before save
         $this->user->save();
 
-        if ( $this->user->id )
-        {
+        if ( $this->user->id ) {
             // Redirect with success message, You may replace "Lang::get(..." for your custom message.
             return Redirect::to('user/login')
                 ->with( 'notice', Lang::get('user/user.user_account_created') );
-        }
-        else
-        {
+        } else {
             // Get validation errors (see Ardent package)
             $error = $this->user->errors()->all();
 
