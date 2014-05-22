2e088e7c915ff65723778806a8d13a4730f009da
natupatu <natupatupato@gmail.com>
2014-05-22 17:41:50 +0200

    View service

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -36,6 +36,9 @@
 		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}}">
 		<link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}}">
 		<link rel="shortcut icon" href="{{{ asset('assets/ico/favicon.png') }}}">
+
+
+        <link rel="stylesheet" href="{{asset('assets/css/hover.css')}}">
 </head>
 
 <body>
