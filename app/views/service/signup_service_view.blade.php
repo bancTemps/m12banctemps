dba1626089af713721d84daae2068cb4ed60aba1
natupatu <natupatupato@gmail.com>
2014-05-23 16:30:01 +0200

    Create service lang

--- app/views/service/signup_service_view.blade.php
+++ app/views/service/signup_service_view.blade.php
@@ -18,50 +18,50 @@
         <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
         <fieldset>
             <div class="form-group">
-                <label for="nom">{{{ Lang::get('confide::confide.servicename') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.servicename') }}}" type="text" name="nom" id="servicename" value="{{{ Input::old('servicename') }}}">
+                <label for="nom">{{{ Lang::get('service.servicename') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.servicename') }}}" type="text" name="nom" id="servicename" value="{{{ Input::old('servicename') }}}">
                 @if($errors->has('nom'))
                 <div class="alert alert-error alert-danger">{{ $errors->first('nom') }}</div>
                 @endif
             </div>
             <div class="form-group">
-                <label for="description">{{{ Lang::get('confide::confide.description') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.description') }}}" type="text" name="descripcio" id="description" value="{{{ Input::old('description') }}}">
+                <label for="description">{{{ Lang::get('service.description') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.description') }}}" type="text" name="descripcio" id="description" value="{{{ Input::old('description') }}}">
                  @if($errors->has('descripcio'))
                 <div class="alert alert-error alert-danger">{{ $errors->first('descripcio') }}</div>
                 @endif
             </div>
             <div class="form-group">
-                <label for="dateI">{{{ Lang::get('confide::confide.dateI') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.dateI') }}}" type="date" name="dataInici" id="dateI">
+                <label for="dateI">{{{ Lang::get('service.date_start') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.date_start') }}}" type="date" name="dataInici" id="dateI">
                 @if($errors->has('dataInici'))
                 <div class="alert alert-error alert-danger">{{ $errors->first('dataInici') }}</div>
                 @endif
             </div>
              <div class="form-group">
-                <label for="dateF">{{{ Lang::get('confide::confide.dateF') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.dateF') }}}" type="date" name="dataFinal" id="dateF">
+                <label for="dateF">{{{ Lang::get('service.date_finish') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.date_finish') }}}" type="date" name="dataFinal" id="dateF">
                 @if($errors->has('dataFinal'))
                     <div class="alert alert-error alert-danger">{{ $errors->first('dataFinal') }}</div>
                 @endif
              </div>
             <div class="form-group">
-                <label for="duration">{{{ Lang::get('confide::confide.duration') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.duration') }}}" type="text" name="duracio" id="duration">
+                <label for="duration">{{{ Lang::get('service.duration') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.duration') }}}" type="text" name="duracio" id="duration">
                 @if($errors->has('duracio'))
                     <div class="alert alert-error alert-danger">{{ $errors->first('duracio') }}</div>
                 @endif
             </div>
             <div class="form-group">
-                <label for="points">{{{ Lang::get('confide::confide.points') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.points') }}}" type="text" name="punts" id="points">
+                <label for="points">{{{ Lang::get('service.points') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.points') }}}" type="text" name="punts" id="points">
                 @if($errors->has('punts'))
                     <div class="alert alert-error alert-danger">{{ $errors->first('punts') }}</div>
                 @endif
             </div>
             <div class="form-group">
-                <label for="city">{{{ Lang::get('confide::confide.city') }}}</label>
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.city') }}}" type="text" name="localitzacio" id="city" value="{{{ Input::old('city') }}}">
+                <label for="city">{{{ Lang::get('service.localization') }}}</label>
+                <input class="form-control" placeholder="{{{ Lang::get('service.localization') }}}" type="text" name="localitzacio" id="city" value="{{{ Input::old('city') }}}">
                 @if($errors->has('localitzacio'))
                     <div class="alert alert-error alert-danger">{{ $errors->first('localitzacio') }}</div>
                 @endif
