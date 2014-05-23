6f69978619d55ae42d007f2c56dcc03017d304f5
tropezoon <paul.cfgs@gmail.com>
2014-05-23 19:43:52 +0200

    Create OtherUserController.php

---
+++ app/controllers/OtherUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+class OtherUserController extends BaseController {
+
+    /**
+     * User Model
+     * @var User
+     */
+    protected $user;
+    
+    /**
+     * Inject the models.
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        parent::__construct();
+        $this->user = $user;
+    }
+
+    public function getIndex() {
+        $userModel = new User;
+        $user = $userModel->getUserByUsername($view);
+        
+        if (is_null($user)){
+            App::abort(404);
+        }
+        
+        return View::make('site/user/login', compact('user'));
+    }
+    public function viewOther($view) {
+        $userModel = new User;
+        $user = $userModel->getUserByUsername($view);
+        
+        if (is_null($user)){
+            App::abort(404);
+        }
+        
+        return View::make('site/user/otherUser', compact('user'));
+    }
+    
+    
+    public function viewOtherService($view) {
+        $userModel = new User;
+        $user = $userModel->getUserByUsername($view);        
+        if (is_null($user)){
+            App::abort(404);
+        }       
+        return View::make('site/user/otherUserService', compact('user'));
+    }
+    
+    
+    public function getOtherServices($slug)
+    {       $userModel = new User;
+            $user = $userModel->getUserByUsername($slug);
+            $services = Service::leftjoin('users', 'users.id', '=', 'services.user_id' )
+                    ->where('users.id','=',$user->id)
+                    ->select(array('services.nom','services.id', 'services.dataInici', 'services.dataFinal','services.punts'));
+
+            return Datatables::of($services)->make();
+
+    }
+}
+
+?>
