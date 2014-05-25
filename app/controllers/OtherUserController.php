19e47dcb095e7dbbcced43f5e397c0d73509549f
tropezoon <paul.cfgs@gmail.com>
2014-05-25 18:59:39 +0200

    añadidas las funciones de denuncia

--- app/controllers/OtherUserController.php
+++ app/controllers/OtherUserController.php
@@ -60,6 +60,49 @@ class OtherUserController extends BaseController {
             return Datatables::of($services)->make();
 
     }
+    
+    // Report method
+    public function reportForm($slug){
+        $userModel = new User;
+        $user = $userModel->getUserByUsername($slug);
+        
+        list($reporter,$redirect) = User::checkAuthAndRedirect('user/messages');
+        if($redirect){return $redirect;}
+        
+        return View::make('site/user/report', compact('user','reporter'));
+    }
+    
+    //POST Report method
+    public function postReport($slug){
+        $userModel = new User;
+        $user = $userModel->getUserByUsername($slug);
+        
+        list($reporter,$redirect) = User::checkAuthAndRedirect('user/messages');
+        if($redirect){return $redirect;}
+        
+        $this->report->emisor_id = $reporter->id;
+        $this->report->receptor_id = $user->id;
+        $this->report->descripcion = Input::get( 'description' );
+        
+        $this->report->save();
+
+        // the data that will be passed into the mail view blade template
+        $data = array(
+            'reporter' => $reporter->username,
+            'reported' => $reported->username,
+            'detail' => $this->report->descripcion,
+        );
+
+        // use Mail::send function to send email passing the data and using the $user variable in the closure
+        Mail::send('emails.report', $data, function($message) use($reporter)
+        {
+          $message->from($reporter->email, $reporter->name);
+          $message->to('paul.cfgs@gmail.com', 'Denuncia nueva');
+        });
+        
+        
+        return View::make('site/user/postReport', compact('user'));
+    }
 }
 
 ?>
