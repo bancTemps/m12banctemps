752e544c21fa5654e56044f28b951c918f010084
tropezoon <paul.cfgs@gmail.com>
2014-05-25 18:39:48 +0200

    Create report.blade.php

---
+++ app/views/emails/report.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h2>A new report was created</h2>
+ 
+    <div>From <b>{{ $reporter }}</b></div>
+    <div>To <b>{{ $reported}}</b> </div>
+    <div>Reason: {{ $detail }}</div>
+  </body>
+</html>
