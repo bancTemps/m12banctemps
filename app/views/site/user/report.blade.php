befd1a2f37e904c6c1d8ef7ed5f2d844cc95daee
tropezoon <paul.cfgs@gmail.com>
2014-05-25 18:42:13 +0200

    denunsia al pinche pendejo!

---
+++ app/views/site/user/report.blade.php
@@ -0,0 +1,36 @@
+@extends('site.layouts.otherUser')
+
+@section('menu-content')
+<form class="form-horizontal" method="POST" action="{{ URL::to('view/'.$user->username.'/report') }}" accept-charset="UTF-8">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <fieldset>
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-md-offset-4 col-md-10">
+                El usuario : <b>{{ $reporter->username }}</b>
+                <input type="hidden" name="reporter" id="reporter" value="{{ $reporter->id }}" />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-md-offset-4 col-md-10">
+                Denuncia a : <b>{{ $user->username }}</b>
+                <input type="hidden" name="reported" id="reported" value="{{ $user->id }}" />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-md-offset-4 col-md-10">
+                Por estas razones:<br/>
+                <input type="textarea" name="description" id="description" cols="80" rows="4"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-md-offset-4 col-md-10">
+                <button tabindex="3" type="submit" class="btn btn-primary">{{ Lang::get('confide::confide.login.submit') }}</button>
+            </div>
+        </div>
+        
+    </fieldset>
+</form>
+@stop
