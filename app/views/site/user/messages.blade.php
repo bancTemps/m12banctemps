9301f6e5f08c00311e2278aa774c73c408a45b0a
natupatu <natupatupato@gmail.com>
2014-05-27 19:32:46 +0200

    Mesages

--- app/views/site/user/messages.blade.php
+++ app/views/site/user/messages.blade.php
@@ -19,16 +19,27 @@
    
 
     <div id="friends" class="col-xs-12 col-sm-3 col-md-4">
-        <?php
-        for ($i=0 ; $i<40 ; $i++) {
-            ?>
-        <div class="message-friend col-xs-12 col-sm-12 col-md-12">
-            <img class="cropper" src="http://img2.wikia.nocookie.net/__cb20140210145556/lovecraft/es/images/7/72/Lovecraft-cthulhu.jpg"/>
-            <p>Cthulhu</p>
-        </div>
-            <?php
-        }
-        ?>
+
+        @if(sizeof($conversations) > 0)
+            @foreach ($conversations as $conversation)
+                <?php  $userModel = new User;
+                    $friend = $userModel->getUserById($conversation->id_receptor)
+                ?>        
+                <div class="col-md-4">
+                    <!-- normal -->
+                    <div class="ih-item square effect3 bottom_to_top">
+                        <a href="#">
+                            <div class="img"><img class="img-responsive" src="{{ asset($friend->photo)}}" alt="img"></div>
+                            <div class="info">
+                              <h3>{{ $friend->username }}</h3>
+                              <p>{{ $friend->name }}</p>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- end normal -->
+                </div>
+            @endforeach                    
+        @endif
     </div>      
 
     <div id="messages" class="col-xs-12 col-sm-5 col-md-5">
@@ -69,6 +80,47 @@
             });
         });
     })(jQuery);
+
+    setInterval(loadLog, 2500);    //Reload file every 2500 ms or x ms if you wish to change the second parameter
+
+    $(document).ready(function(){
+        $("#exit").click(function(){
+            alertify.confirm("Seguro que quieres cerrar sesión? Est acción borrará la sala.", function (e) {
+                if (e) {
+                    window.location = '../index.php?logout=true';
+                } 
+            });
+        });
+    });
+
+    $("#submitmsg").click(function(){   
+        var clientmsg = $("#usermsg").val();
+        // jquery post request: envia un valor post. 
+        $.post("../controller/enviar_mensaje.php", {
+            text: clientmsg
+        });              
+        $("#usermsg").attr("value", "");
+
+        return false;
+    });
+
+    function loadLog(){     
+        var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+
+        $.ajax({
+            type : 'POST',
+            url : '../controller/imprimir_chat.php',
+            success:function (data) {
+                $("#chatbox").html(data);
+                //Auto-scroll           
+                var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+                if(newscrollHeight > oldscrollHeight){
+                    $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                }  
+            }          
+        }); 
+    }
+
 </script>
 
 
