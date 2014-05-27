5a46162d8ea5677ca06095f22e514a36c9bf18d2
natupatu <natupatupato@gmail.com>
2014-05-27 15:38:26 +0200

    Search modificado

--- app/views/site/search.blade.php
+++ app/views/site/search.blade.php
@@ -12,38 +12,59 @@
     
     <!--Contenido-->
     <div class="container">
-        <h2>Serveis </h2>
+        <div class="page-header">
+            <h2>Serveis </h2>
+        </div>
         
         
         
         <div class="col-md-12">
-            <div class="row">   
+            <div class="row"> 
+                <h3>Resultados de la búsqueda: <strong>{{$search}}</strong></h3>  
+
                 @if(sizeof($services) > 0)
-                @foreach ($services as $service)          
-                 <div class="col-md-4">
-                    <!-- normal -->
-                    <div class="ih-item square effect3 bottom_to_top">
-                        <a href="{{ $service->url() }}">
-                            <div class="img"><img class="img-responsive" src="http://placekitten.com/300/200" alt="img"></div>
-                            <div class="info">
-                              <h3>{{ $service->nom }}</h3>
-                              <p>{{ $service->descripcio }}</p>
+                    @foreach ($services as $service)          
+                    <div class="col-md-4">
+                        <!-- normal -->
+                        <div class="ih-item square effect3 bottom_to_top">
+                            <a href="{{ $service->url() }}">
+                                <div class="img"><img class="img-responsive" src="http://placekitten.com/300/200" alt="img"></div>
+                                <div class="info">
+                                  <h3>{{ $service->nom }}</h3>
+                                  <p>{{ $service->descripcio }}</p>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- end normal -->
+                      </div>
+                    @endforeach                    
+                @else
+                    <p>No existe un servicio con ese nombre</p>
+                @endif
+
+                @if(sizeof($users) > 0)
+                    <h3>Resultados de la búsqueda: <strong>{{$search}}</strong></h3>
+                    @foreach ($users as $user)          
+                        <div class="col-md-4">
+                            <!-- normal -->
+                            <div class="ih-item square effect3 bottom_to_top">
+                                <a href="{{ $user->username }}">
+                                    <div class="img"><img class="img-responsive" src="http://placekitten.com/300/200" alt="img"></div>
+                                    <div class="info">
+                                      <h3>{{ $user->username }}</h3>
+                                      <p>{{ $user->points }}</p>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
-                    <!-- end normal -->
-                  </div>
-                @endforeach
+                        <!-- end normal -->
+                        </div>
+                    @endforeach
+                @else
+                    <p>No existe un usuario con ese nombre</p>
                 @endif
             </div>    
         </div>
         
     </div>
     
-@stop
-
-{{-- scripts --}}
-@section('scripts')
-    {{ HTML::script('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false'); }}
-    {{ HTML::script('assets/js/gmaps.js'); }}
 @stop
